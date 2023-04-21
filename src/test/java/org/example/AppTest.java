a99fe14bc9f04df84a14965567484177e1c65fee
nitu <ns21709npj@gmail.com>
2023-04-21 11:18:32 +0530

    first commit

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,38 +1,59 @@
 package org.example;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.util.List;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+    ChromeDriver driver;
+    @BeforeClass
+    void setupDriver () {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        driver.get("https://www.webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
+        driver.manage().window().maximize();
+    } @Test()
+    void Webdriverdropdownlogin() throws InterruptedException {
+        Thread.sleep(19000);
+
+
+        Select dropdown = new Select(driver.findElement(By.xpath("//*[@id=\"dropdowm-menu-1\"]")));
+        dropdown.selectByVisibleText("Python");
+
+        Select dropdown1 = new Select(driver.findElement(By.xpath("//*[@id=\"dropdowm-menu-2\"]")));
+
+        dropdown1.selectByVisibleText("TestNG");
+        Thread.sleep(1000);
+
+        Select dropdown2 = new Select(driver.findElement(By.xpath("//*[@id=\"dropdowm-menu-3\"]")));
+        dropdown2.selectByVisibleText("JavaScript");
+        Thread.sleep(1000);
+
+
+
+        WebElement checkbox2 = driver.findElement(By.cssSelector("input[value='option-2']"));
+        checkbox2.click();
+        Thread.sleep(1000);
+
+        WebElement radioButton = driver.findElement(By.cssSelector("input[value='yellow']"));
+        radioButton.click();
+        Thread.sleep(1000);
+
+
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    @AfterClass
+    void closeDriver () {
+        driver.close();
+}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
